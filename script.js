3d07213fb12cb57be33c660a9f506024e73b0d43
timothyda <timdalden@gmail.com>
2022-10-28 15:40:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -24,10 +24,10 @@
             var randomNumber2 = Math.floor(Math.random() * 6) + 1;
             
             document.querySelector(".img1").setAttribute("src",
-                "dice" + randomNumber1 + ".png");
+                "Dice" + randomNumber1 + ".png");
             
             document.querySelector(".img2").setAttribute("src",
-                "dice" + randomNumber2 + ".png");
+                "Dice" + randomNumber2 + ".png");
             
             if (randomNumber1 === randomNumber2) {
                 document.querySelector("h2").innerHTML = "Draw!";
