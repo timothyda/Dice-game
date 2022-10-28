44aa7f40bde8bb10e6ab863e2046efd391469d4d
timothyda <timdalden@gmail.com>
2022-10-28 15:33:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,49 @@
-//sets dice side images as veriable
-let img= ["Dice1.png","Dice2.png","Dice3.png",
-"Dice4.png","Dice5.png","Dice6.png"];
+document.addEventListener("DOMContentLoaded", function(event) { 
 
-//associates dice with img 
-let dice= document.querySelectorAll("img");
+    //sets dice side images as veriable
+    let img= ["Dice1.png","Dice2.png","Dice3.png",
+    "Dice4.png","Dice5.png","Dice6.png"];
 
-//shake dice
-function rollTheDice() {
-    dice.forEach(function(die){
-        die.classList.add("shake");
-    });
+    //associates dice with img 
+    let dice= document.querySelectorAll("img");
 
-
-    /* timeout for shake, picks random number between 1-6, 
-    associates that with dice side, and displays winner */
-    setTimeout(function(){
+    //shake dice
+    function rollTheDice() {
         dice.forEach(function(die){
-            die.classList.remove("shake");
+            die.classList.add("shake");
         });
-        var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-        var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-        
-        document.querySelector(".img1").setAttribute("src",
-            "dice" + randomNumber1 + ".png");
-        
-        document.querySelector(".img2").setAttribute("src",
-            "dice" + randomNumber2 + ".png");
-        
-        if (randomNumber1 === randomNumber2) {
-            document.querySelector("h2").innerHTML = "Draw!";
-        }
-        
-        else if (randomNumber1 < randomNumber2) {
-            document.querySelector("h2").innerHTML
-                                    = (player2 + " WINS!");
-        }
-        
-        else {
-            document.querySelector("h2").innerHTML
-                                    = (player1 + " WINS!");
-        }
-        
-    }, 1200 //time for shake timeout
-    );
-}
+
+
+        /* timeout for shake, picks random number between 1-6, 
+        associates that with dice side, and displays winner */
+        setTimeout(function(){
+            dice.forEach(function(die){
+                die.classList.remove("shake");
+            });
+            var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+            var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+            
+            document.querySelector(".img1").setAttribute("src",
+                "dice" + randomNumber1 + ".png");
+            
+            document.querySelector(".img2").setAttribute("src",
+                "dice" + randomNumber2 + ".png");
+            
+            if (randomNumber1 === randomNumber2) {
+                document.querySelector("h2").innerHTML = "Draw!";
+            }
+            
+            else if (randomNumber1 < randomNumber2) {
+                document.querySelector("h2").innerHTML
+                                        = (player2 + " WINS!");
+            }
+            
+            else {
+                document.querySelector("h2").innerHTML
+                                        = (player1 + " WINS!");
+            }
+            
+        }, 1200 //time for shake timeout
+        );
+    }
+});
