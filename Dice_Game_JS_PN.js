b996c99dbc8e3792b3f492e68a5ae8a7da7d6d4b
timothyda <timdalden@gmail.com>
2022-10-28 13:41:20 -0400

    Dice game
    Dice game

---
+++ Dice_Game_JS_PN.js
@@ -0,0 +1,14 @@
+  
+    // Player name
+    var player1 = "Player 1";
+    var player2 = "Player 2";
+  
+    // Function to change the player name
+    function editNames(){
+        player1 = prompt("Change Player1 name");
+        player2 = prompt("Change player2 name");
+
+        document.querySelector("p.Player1").innerHTML = player1;
+        document.querySelector("p.Player2").innerHTML = player2;
+    }  
+    
---
+++ Dice_Game_JS_RD.js
@@ -0,0 +1,44 @@
+// Calls images for side of dice
+let images= ["Dice1.png","Dice2.png","Dice3.png",
+"Dice4.png","Dice5.png","Dice6.png"];
+let dice= document.querySelectorAll("img");
+
+// Function starts to shake dice
+function rollTheDice() {
+    dice.forEach(function(die){
+        die.classList.add("shake");
+    });
+
+    /* setTimeout for shake, Picks random number between 1-6, and 
+    calls that number associated with the dice#.png */
+    setTimeout(function(){
+        dice.forEach(function(die){
+            die.classList.remove("shake");
+        });
+        var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+        var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+        
+        document.querySelector(".img1").setAttribute("src",
+            "dice" + randomNumber1 + ".png");
+        
+        document.querySelector(".img2").setAttribute("src",
+            "dice" + randomNumber2 + ".png");
+        
+        // Displays winner at h2, above the dice
+        if (randomNumber1 === randomNumber2) {
+            document.querySelector("h2").innerHTML = "Draw!";
+        }
+        
+        else if (randomNumber1 < randomNumber2) {
+            document.querySelector("h2").innerHTML
+                                    = (player2 + " WINS!");
+        }
+        
+        else {
+            document.querySelector("h2").innerHTML
+                                    = (player1 + " WINS!");
+        }
+        
+    }, 1200 //time for setTimeout
+    );
+}
