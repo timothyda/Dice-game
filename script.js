eb83a6ed809c3c7870fb546aa1898c9947a97134
timothyda <timdalden@gmail.com>
2022-10-28 15:47:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,45 @@
-    //sets dice side images as veriable
-    let img= ["Dice1.png","Dice2.png","Dice3.png",
-    "Dice4.png","Dice5.png","Dice6.png"];
+//sets dice side images as veriable
+let img= ["Dice1.png","Dice2.png","Dice3.png",
+"Dice4.png","Dice5.png","Dice6.png"];
 
-    //associates dice with img 
-    let dice= document.querySelectorAll("img");
+//associates dice with img 
+let dice= document.querySelectorAll("img");
 
-    //shake dice
-    function rollTheDice() {
-        dice.forEach(function(die){
-            die.classList.add("shake");
-        });
+//shake dice
+function rollTheDice() {
+    dice.forEach(function(die){
+        die.classList.add("shake");
+});
 
 
-        /* timeout for shake, picks random number between 1-6, 
-        associates that with dice side, and displays winner */
-    setTimeout(function(){
-        dice.forEach(function(die){
-        die.classList.remove("shake");
-        });
+setTimeout(function(){
+    dice.forEach(function(die){
+    die.classList.remove("shake");
+    });
 
-        var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-        var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
             
-        document.querySelector(".img1").setAttribute("src",
-            "Dice" + randomNumber1 + ".png");
+    document.querySelector(".img1").setAttribute("src",
+        "Dice" + randomNumber1 + ".png");
             
-        document.querySelector(".img2").setAttribute("src",
-            "Dice" + randomNumber2 + ".png");
+    document.querySelector(".img2").setAttribute("src",
+        "Dice" + randomNumber2 + ".png");
             
-        if (randomNumber1 === randomNumber2) {
-            document.querySelector("h2").innerHTML = "Draw!";
-        }
+    if (randomNumber1 === randomNumber2) {
+        document.querySelector("h2").innerHTML = "Draw!";
+    }
             
-        else if (randomNumber1 < randomNumber2) {
-            document.querySelector("h2").innerHTML
+    else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h2").innerHTML
                                     = (player2 + " WINS!");
-        }
+    }
             
-        else {
-            document.querySelector("h2").innerHTML
+    else {
+        document.querySelector("h2").innerHTML
                                     = (player1 + " WINS!");
-         }
+    }
             
-    }, 1200 //time for shake timeout
-    );
-   }
+}, 1200 //time for shake timeout
+);
+}
