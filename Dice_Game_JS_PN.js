0160b7e2664c18757a9635abbe9761a100870762
timothyda <timdalden@gmail.com>
2022-10-28 15:18:46 -0400

    attempt

--- Dice_Game_JS_PN.js
+++ Dice_Game_JS_PN.js
@@ -1,9 +1,8 @@
   
-    // Player name
     var player1 = "Player 1";
     var player2 = "Player 2";
   
-    // Function to change the player name
+
     function editNames(){
         player1 = prompt("Change Player1 name");
         player2 = prompt("Change player2 name");
