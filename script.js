bb6dc6ad003ad2ad0c1e6c0f9ffb1c3d0ef81557
timothyda <timdalden@gmail.com>
2022-12-14 21:19:00 -0500

    Dice_JS_Changes

--- script.js
+++ script.js
@@ -43,3 +43,18 @@ setTimeout(function(){
 }, 1200 //time for shake timeout
 );
 }
+
+
+  //set-Player names
+var player1 = "Player 1";
+var player2 = "Player 2";
+
+//change names
+function editNames(){
+    player1 = prompt("Change Player1 name");
+    player2 = prompt("Change player2 name");
+
+    document.querySelector("p.Player1").innerHTML = player1;
+    document.querySelector("p.Player2").innerHTML = player2;
+} 
+
